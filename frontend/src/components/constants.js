6701b1ca38dd2bbc12ff0467f3e2c9913f120ddb
Mahad Ali <mahad107600@gmail.com>
2025-07-29 22:59:42 +0500

    Update constants.js

--- frontend/src/components/constants.js
+++ frontend/src/components/constants.js
@@ -1,19 +1,9 @@
 export const SERVICES = [
-  "Web Design",
-  "Web Development",
-  "Web App",
+  "Design",
+  "Development",
+  "Branding",
+  "Video Editing",
+  "Marketing",
   "3D",
-  "Shopify",
   "Other",
 ];
-
-export const BANNER_WORDS = [
-  "Sleek",
-  "Innovative",
-  "Sophisticated",
-  "Dynamic",
-  "Elegant",
-];
-
-// Repeat the words array 10 times for a very long marquee
-export const BANNER_TEXT = Array(50).fill(BANNER_WORDS).flat().join("   •   ");
